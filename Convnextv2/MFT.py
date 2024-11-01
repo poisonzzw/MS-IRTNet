7fb70c1594d3af4599bf7d71d400282376ec5707
poisonzzw <41930704+poisonzzw@users.noreply.github.com>
2024-11-01 11:22:41 +0800

    Update MFT.py

--- Convnextv2/MFT.py
+++ Convnextv2/MFT.py
@@ -18,18 +18,18 @@ class CrossAttention(nn.Module):
 
     def forward(self, x1, x2):
         B, N, C = x1.shape
-        q1 = x1.reshape(B, -1, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3).contiguous()
-        q2 = x2.reshape(B, -1, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3).contiguous()
-        k1, v1 = self.kv1(x1).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4).contiguous()
-        k2, v2 = self.kv2(x2).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4).contiguous()
+        v1 = x1.reshape(B, -1, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3).contiguous()
+        v2 = x2.reshape(B, -1, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3).contiguous()
+        k1, q1 = self.kv1(x1).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4).contiguous()
+        k2, q2 = self.kv2(x2).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4).contiguous()
 
-        ctx1 = (k1.transpose(-2, -1) @ v1) * self.scale
+        ctx1 = (k1.transpose(-2, -1) @ q1) * self.scale
         ctx1 = ctx1.softmax(dim=-2)
-        ctx2 = (k2.transpose(-2, -1) @ v2) * self.scale
+        ctx2 = (k2.transpose(-2, -1) @ q2) * self.scale
         ctx2 = ctx2.softmax(dim=-2)
 
-        x1 = (q1 @ ctx2).permute(0, 2, 1, 3).reshape(B, N, C).contiguous()
-        x2 = (q2 @ ctx1).permute(0, 2, 1, 3).reshape(B, N, C).contiguous()
+        x1 = (v1 @ ctx2).permute(0, 2, 1, 3).reshape(B, N, C).contiguous()
+        x2 = (v2 @ ctx1).permute(0, 2, 1, 3).reshape(B, N, C).contiguous()
 
         return x1 , x2
 
